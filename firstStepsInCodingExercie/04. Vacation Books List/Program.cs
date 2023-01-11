b0b5c012f60f96599fa007ff11bf1352f46d93f2
Bratanov <111681891+ybratanov@users.noreply.github.com>
2023-01-11 10:50:36 +0100

    Add files via upload

---
+++ firstStepsInCodingExercie/04. Vacation Books List/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int page = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            int totalHour = page / pagesPerHour;
+
+            int hoursPerDay = totalHour/ days;
+
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
