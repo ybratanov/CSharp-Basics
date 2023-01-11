6fd313e889a0fb411accae208df3ad13524bd753
Bratanov <111681891+ybratanov@users.noreply.github.com>
2023-01-11 11:27:24 +0100

    Add files via upload

---
+++ Conditional Statements - Exercise/01. Sum Seconds/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Sum_Seconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int secund = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int time = first + secund + third;
+            int min = time / 60;
+            int sec = time % 60;
+
+            if ( sec < 10)
+            {
+                Console.WriteLine($"{min}:0{sec}");
+            }
+            else
+            {
+                Console.WriteLine($"{min}:{sec}");
+            }
+        }
+    }
+}
